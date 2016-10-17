4430be441af7e8b1db7e905f98b8b18757fdb13c
Anastasiya <anastkononova@gmail.com>
2016-10-17 14:31:39 +0300

    fixed bugs in client

--- client.c
+++ client.c
@@ -7,14 +7,14 @@
 int main ()
 {
 	char message[2048];
-	char buf[sizeof(message)];
+	char bufer[sizeof(message)];
 	int ch;
  
-	int sock;           
+	int sockets;           
 	struct sockaddr_in addr;
 
-    	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock < 0){
+    	sockets = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockets < 0){
 		perror("Ошибка socket\n");
 		exit(1);
 	}
@@ -23,17 +23,17 @@ int main ()
     	addr.sin_family = AF_INET;
     	addr.sin_port = htons(3425);
     
-   	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+   	if(connect(sockets, (struct sockaddr *)&addr, sizeof(addr)) < 0){
 		perror("Ошибка подключения\n");
 		exit(2);
 	}
  
 	printf("Введите команду: ");
 	fgets(message, 2048, stdin);
-	send(sock, message, sizeof(message), 0);
-	int bytes_read = 0;
-	bytes_read = recv(sock, buf, sizeof(message), 0);
-	printf("Результат: %s\n", buf);
+	send(sockets, message, sizeof(message), 0);
+	int read = 0;
+	read = recv(sockets, bufer, sizeof(message), 0);
+	printf("Результат: %s\n", bufer);
     
 	return 0;
 }
